cfff1e872bddf7033e33fa58eea2481092435950
Raitus <rd.raitus@gmail.com>
2021-07-02 13:12:42 +0300

    changed mistakes after revision

--- main.cpp
+++ main.cpp
@@ -7,28 +7,31 @@ int main() {
   std::cout << "Input second number: ";
   std::cin >> secondNumber;
 
-  if (firstNumber.length() < 5 && secondNumber.length() < 5 && firstNumber[0] - '0' >= 0 && firstNumber[0] - '0' <= 9
-	  && firstNumber[1] - '0' >= 0 && firstNumber[1] - '0' <= 9 && firstNumber[2] - '0' >= 0
-	  && firstNumber[2] - '0' <= 9 && firstNumber[3] - '0' >= 0 && firstNumber[3] - '0' <= 9
-	  && secondNumber[0] - '0' >= 0 && secondNumber[0] - '0' <= 9 && secondNumber[1] - '0' >= 0
-	  && secondNumber[1] - '0' <= 9 && secondNumber[2] - '0' >= 0 && secondNumber[2] - '0' <= 9
-	  && secondNumber[3] - '0' >= 0 && secondNumber[3] - '0' <= 9) {
+  if (firstNumber.length()==4 && secondNumber.length()==4 && firstNumber[0] >= '0' && firstNumber[0] <= '9'
+	  && firstNumber[1] >= '0' && firstNumber[1] <= '9' && firstNumber[2] >= '0'
+	  && firstNumber[2] <= '9' && firstNumber[3] >= '0' && firstNumber[3] <= '9'
+	  && secondNumber[0] >= '0' && secondNumber[0] <= '9' && secondNumber[1] >= '0'
+	  && secondNumber[1] <= '9' && secondNumber[2] >= '0' && secondNumber[2] <= '9'
+	  && secondNumber[3] >= '0' && secondNumber[3] <= '9') {
 	int cow = 0, bull = 0;
 	for (int i = 0; i < firstNumber.length(); i++) {
 	  if (firstNumber[i]==secondNumber[i]) {
 		bull++;
-	  }
-	  for (int y = 0; y < secondNumber.length(); y++) {
-		if (firstNumber[i]==secondNumber[y] && firstNumber[i]!=secondNumber[i] && secondNumber[y]!=firstNumber[y]) {
-		  cow++;
+	  }else if (i==0 || (i==1 && firstNumber[i]!=firstNumber[0])
+		  || (i==2 && firstNumber[i]!=firstNumber[0] && firstNumber[i]!=firstNumber[1])
+		  || (i==3 && firstNumber[i]!=firstNumber[0] && firstNumber[i]!=firstNumber[1]
+			  && firstNumber[i]!=firstNumber[2])) {
+		for (int y = 0; y < secondNumber.length(); y++) {
+		  if (firstNumber[i]==secondNumber[y] && firstNumber[i]!=secondNumber[i] && secondNumber[y]!=firstNumber[y]) {
+			cow++;
+			break;
+		  }
 		}
 	  }
 	}
-	std::cout << "Bulls: " << bull << ", cows: " << cow << ".\n" << firstNumber.length() << std::endl
-			  << secondNumber.length();
+	std::cout << "Bulls: " << bull << ", cows: " << cow << ".\n";
   } else {
 	std::cout << "Error! Please, input number in the right format.\n";
   }
-
   return 0;
 }
